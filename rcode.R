df070f5774355114c94f2ad928874d3dcdcd13a4
davidjayjackson <davidjayjackson@gmail.como>
2021-01-16 13:49:30 -0500

    Updated 2021-01-16

--- rcode.R
+++ rcode.R
@@ -1,16 +1,4 @@
-# library(RSQLite)
 library(ggplot2)
-library(scales)
-library(dplyr)
-library(readr)
-###
-df <- read_csv("./data.csv")  %>% filter(geoId == "US") %>% 
-select(dateRep, cases_weekly, deaths_weekly, geoId) %>%
-rename(Ymd = dateRep,
-Cases = cases_weekly,
-Deaths = deaths_weekly)
-df$Ymd <- as.Date(df$Ymd, format = "%d-%m-%Y")
-###
-
+x <- c(1,3,5,7,9)
+y <- c(2,4,6,8,10)
 ggplot()+ geom_col(data = df, aes( x = Ymd, y = Cases))
-ggplot()+ geom_col(data = df, aes( x = Ymd, y = Deaths))
